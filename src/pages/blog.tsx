bdc20949966359a3efe7e94c08d1d95f8b9da814
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-02-21 22:32:32 -0300

    feat:paginateData

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,48 +1,29 @@
-import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
-import React from "react";
+import { Box, Flex, HStack, SimpleGrid } from "@chakra-ui/react";
+import React, { useCallback, useMemo, useState } from "react";
 import { Header } from "../components/Header";
 import { FieldSearch } from "../components/FieldSearch/inde";
 import { GetStaticProps } from "next";
 import { ButtonTheme } from "../components/ButtonTheme";
 import { supabase } from "../lib/initSupabase";
-import { PostgrestError } from "@supabase/supabase-js";
 import { Card } from "../components/Card";
 import { Limiter } from "../components/Limiter";
 import { format } from "date-fns";
 import FirstCard from "../components/FirstCard";
-import { last } from "lodash";
-
-interface ThemeData {
-  allThemes: {
-    data: Array<{
-      id: number;
-      theme: string | null;
-    }>;
-    error: PostgrestError | null;
-  };
-  allPosts: {
-    data: Array<{
-      id: number;
-      thumbnail: {
-        id: number;
-        thumbnail_url: string;
-      };
-      theme: {
-        id: number;
-        theme: string;
-      };
-      title: string | null;
-      author: string | null;
-      content: string | null;
-      description: string | null;
-      created_at: string;
-    }>;
-    error: PostgrestError | null;
-  };
-}
+import { last, orderBy } from "lodash";
+import { ThemeData } from "../@types/types";
 
 export default function BlogPage(themesData: ThemeData) {
-  /* console.log(themesData?.allPosts.data.at(-1)); */
+  const orderedData = useMemo(() => {
+    return orderBy(themesData.allPosts.data, "created_at");
+  }, [themesData.allPosts]);
+
+  const [loadedPosts, setLoadedPosts] = useState(6);
+
+  const visiblePosts = orderedData.slice(0, loadedPosts);
+
+  const loadMorePosts = useCallback(() => {
+    setLoadedPosts((prev) => prev + 6);
+  }, []);
 
   return (
     <Limiter>
@@ -67,9 +48,9 @@ export default function BlogPage(themesData: ThemeData) {
         <FieldSearch name="fieldsearch" />
       </Flex>
 
-      <Flex gap={'0.5rem'} justifyContent="center">
+      <Flex gap={"0.5rem"} justifyContent="center">
         {!themesData.allThemes.error &&
-          themesData.allThemes.data?.map((themes, index) => {
+          themesData?.allThemes?.data?.map((themes, index) => {
             return (
               <ButtonTheme key={`themesButton-${index}`}>
                 {themes.theme}
@@ -79,9 +60,8 @@ export default function BlogPage(themesData: ThemeData) {
       </Flex>
 
       <Flex my="2rem">
-        {!themesData.allThemes.error &&
-          themesData.allPosts.data &&
-          [last(themesData.allPosts.data)].map((lastItem) => {
+        {!themesData.allPosts.error &&
+          [last(themesData?.allPosts?.data)].map((lastItem) => {
             if (lastItem) {
               return (
                 <FirstCard
@@ -91,15 +71,15 @@ export default function BlogPage(themesData: ThemeData) {
                   title={lastItem.title ?? ""}
                   description={lastItem.description ?? ""}
                   author={lastItem.author ?? "Thiago Maurat"}
-                  date={format(new Date(lastItem.created_at), "dd-MM-yyyy, 'às' HH:mm.")}
+                  date={format(
+                    new Date(lastItem.created_at),
+                    "dd-MM-yyyy, 'às' HH:mm."
+                  )}
                 />
               );
             }
           })}
       </Flex>
-
-
-
 
       {!themesData.allPosts.error && (
         <SimpleGrid
@@ -107,8 +87,9 @@ export default function BlogPage(themesData: ThemeData) {
           justifyItems={"center"}
           gap={"3rem"}
           columns={{ base: 1, md: 2, xl: 3 }}
+          mb="2rem"
         >
-          {themesData.allPosts.data?.map((posts, index) => {
+          {visiblePosts?.map((posts, index) => {
             return (
               <Card
                 key={`card-posts${index}`}
@@ -126,11 +107,19 @@ export default function BlogPage(themesData: ThemeData) {
           })}
         </SimpleGrid>
       )}
+
+      {loadedPosts < orderedData.length && (
+        <HStack justifyContent={"center"} my="2rem">
+          <ButtonTheme onClick={loadMorePosts}>Carregar mais</ButtonTheme>
+        </HStack>
+      )}
     </Limiter>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  // fetch all data on server (revalidate) and paginate on front
+
   const { data: themesData, error: themesError } = await supabase
     .from("themes")
     .select("id, theme");
@@ -143,13 +132,11 @@ export const getStaticProps: GetStaticProps = async () => {
           theme:themes(id, theme)`
     );
 
-  /* const revalidate = 60 * 60 * 24; */ // cache for 24 hours
-
   return {
     props: {
       allThemes: { data: themesData, error: themesError },
       allPosts: { data: postsData, error: postsError },
     },
-    /* revalidate: revalidate, */
+    /* revalidate: 60 * 60 * 24 * 2,  */
   };
 };
